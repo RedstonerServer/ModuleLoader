507f11577d67e32070460b9304dc81c690d2250e
Pepich <benedikt.abel@yahoo.de>
2017-03-10 13:46:31 +0100

    Initial commit for planned modules

---
+++ src/com/redstoner/modules/chatonly/ChatOnly.java
@@ -0,0 +1,8 @@
+package com.redstoner.modules.chatonly;
+
+import com.redstoner.annotations.Version;
+import com.redstoner.modules.Module;
+
+@Version(major = 3, minor = 0, revision = 0, compatible = 3)
+public class ChatOnly implements Module
+{}
---
+++ src/com/redstoner/modules/essentials/Essentials.java
@@ -0,0 +1,8 @@
+package com.redstoner.modules.essentials;
+
+import com.redstoner.annotations.Version;
+import com.redstoner.modules.Module;
+
+@Version(major = 3, minor = 0, revision = 0, compatible = 3)
+public class Essentials implements Module
+{}
